85fe8fa9223658e4aefed987cd7839367d166fcf
chatthong <chatthong_hon@jobbkk.co.th>
2021-09-28 11:29:37 +0700

    uuu

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import colors from "vuetify/es5/util/colors";
 
 export default {
   target: "server",
+  router: {
+    base: "/portfolio/"
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - portfolio",
