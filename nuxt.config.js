222ee3a486b6e17635e4177e5e689d524690e408
chatthong <chatthong_hon@jobbkk.co.th>
2021-10-01 10:46:43 +0700

    qqq

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  target: "server",
-  // target: "static",
+  // target: "server",
+  target: "static",
   router: {
     base: "/portfolio/"
   },
