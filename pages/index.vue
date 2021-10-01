b25f9228aeb977b980d79d563f4d9e235f860384
chatthong <chatthong_hon@jobbkk.co.th>
2021-10-01 13:16:02 +0700

    eeeeey

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@
     <present />
     <favorite />
     <workHistory />
+    <h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam eius
+      sit, illo dolores explicabo, earum maiores optio itaque sequi eos
+      excepturi quas ipsam obcaecati culpa eligendi vel. Iste, ullam?
+      Consequatur.
+    </h1>
   </div>
 </template>
 
